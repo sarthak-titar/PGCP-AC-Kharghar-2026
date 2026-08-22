fa76dd06db7ac2e02452eb3fed5a80b753cd3292
Sarthak Titar <sarthaktitar@gmail.com>
2026-08-22 20:03:43 +0530

    Add files via upload

---
+++ Logic Building/Day5/patterns/pattern7.java
@@ -0,0 +1,13 @@
+class pattern7{
+	public static void main(String args[]){
+		int sum = 0;
+		for (int i = 1 ; i<=5;i++){
+			for (int j = 1; j<=i ;j++){
+				sum= sum +1;
+				System.out.print(sum);
+				
+			}
+			System.out.println();
+		}
+	}
+}
