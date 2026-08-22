bde9f9ae20cf0af01117878cdeef4b7934f7dd48
Sarthak Titar <sarthaktitar@gmail.com>
2026-08-22 20:04:50 +0530

    Add files via upload

---
+++ Logic Building/Day5/patterns/pattern9.java
@@ -0,0 +1,31 @@
+class pattern9{
+	public static void main(String args[]){
+		
+		for (int i = 1 ; i<=6;i++){
+			for(int k = i; k<6;k++){
+					System.out.print(" ");
+			}
+			for (int j = 1; j<=2*i-1;j++){
+				System.out.print("*");
+				
+				}
+				
+			
+			System.out.println();
+		}
+		
+				for (int i = 5 ; i>=1;i--){
+			for(int k = i; k<6;k++){
+					System.out.print(" ");
+			}
+			for (int j = 1; j<=2*i-1;j++){
+				System.out.print("*");
+				
+				}
+				
+			
+			System.out.println();
+				}
+		
+	}
+}
