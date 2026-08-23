216c70541bc3a8a7fa9e17351d3e385f4b268bdf
Sarthak Titar <sarthaktitar@gmail.com>
2026-08-23 23:23:26 +0530

    Add files via upload

---
+++ Logic Building/Day5/String/Literal.java
@@ -0,0 +1,11 @@
+class Literal{
+	public static void main(String args[]){
+		String str1 = "hello";
+		String str2 = "hello";
+		if(str1 == str2){
+			System.out.println("true");
+		}
+		System.out.println(str1 == str2);
+		
+	}
+}
