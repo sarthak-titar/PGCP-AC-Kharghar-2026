9e8d305eea7c4dccacf0cc82e41486fe2aac7b20
Sarthak Titar <sarthaktitar@gmail.com>
2026-08-24 23:05:57 +0530

    -

---
+++ cpp/Day1/additionoftwonumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b;
+
+	cout<<"Enter two numbers";
+	cin>>  a;
+	cin>>b;
+	int sum = a+b;
+	cout<<"sum of two number "<<sum;
+	return 0 ;
+}
