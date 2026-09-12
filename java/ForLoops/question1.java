072e095e85e1a3c5f72c83e591788daae7736cee
Sarthak Titar <sarthaktitar@gmail.com>
2026-09-12 23:33:48 +0530

    print multiples of 7 up to 200

---
+++ java/ForLoops/question1.java
@@ -0,0 +1,23 @@
+
+public class After7 {
+	
+		void After7numm(int n) {
+			
+			for(int i = 1 ; i <= n ; i++) {
+				int num = i*7;
+				if(num <= 200) {
+				System.out.println(num);
+				}
+				
+			}		
+		}	
+		public static void main(String[] args) {
+		
+					
+			
+			int n = 200;
+		new After7().After7numm(n);
+		}
+	
+
+}
