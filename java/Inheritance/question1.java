b6c514fd91fb7e8ddb677554df8257a32ac52d3b
Sarthak Titar <sarthaktitar@gmail.com>
2026-09-13 21:04:07 +0530

    Implement inheritance with parent and child classes

---
+++ java/Inheritance/question1.java
@@ -0,0 +1,31 @@
+//1. Create a class with a method that prints "This is parent class" and its subclass 
+//with another method that prints "This is child class". Now, create an object for 
+//each of the class and call
+//1 - method of parent class by object of parent class
+//2 - method of child class by object of child class
+//3 - method of parent class by object of child class
+class parent{
+	void show1() {
+		System.out.println("This is parent class");	
+		}	
+}
+class child extends parent{
+	void show() {
+		System.out.println("This is child class");	
+		
+	}
+	
+}
+
+public class inheritance {
+
+	public static void main(String[] args) {
+		parent p = new parent();
+		p.show1();
+		child c = new child();
+		c.show();	
+		c.show1();
+
+	}
+
+}
