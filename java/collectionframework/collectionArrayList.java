481b9befe7b45e53b7037fd3bc0e5e4984f82cdb
Sarthak Titar <sarthaktitar@gmail.com>
2026-09-19 23:59:21 +0530

    Create CollectionFramework class with ArrayList example
    Implemented a Java program using ArrayList to manage a collection of colors with various operations.

---
+++ java/collectionframework/collectionArrayList.java
@@ -0,0 +1,23 @@
+package collection;
+import java.util.ArrayList;
+//1. Write a Java program to create a new array list, add some colors (string) and print out the collection.
+//2. Modify the above Java program to insert an element into the array list at the first position.
+//3. Modify the above Java program to retrieve an element (at a specified index) from a given array list.
+//4. Modify the above Java program to update specific array element by given element.
+//5. Modify the above Java program to remove the third element from a array list.
+//6. Modify the above Java program to search an element in a array list
+
+public class CollectionFramework {
+	public static void main(String[] args) {
+		
+		ArrayList<String> arraylist = new ArrayList<>(10);
+		
+		
+		arraylist.add("RED");
+		System.out.println(arraylist);
+		
+		
+		
+	}
+
+}
